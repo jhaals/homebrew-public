2599bea85d48466b58900ad0263ffb2e28915fb4
Johan Haals <johan.haals@gmail.com>
2018-03-28 10:07:07 +0200

    remove deps, change app name

--- protoman.rb
+++ protoman.rb
@@ -4,9 +4,8 @@ class Protoman < Formula
     url "https://github.com/spotify/protoman/releases/download/0.0.1/protoman-darwin-amd64.zip"
     version "0.0.1"
     sha256 "451a43026d415bb709af6ac1eba9b4be1af4c3ad8e4b530773436693a32a051a"
-    depends_on "git"
 
     def install
-      bin.install "program"
+      bin.install "protoman"
     end
   end
