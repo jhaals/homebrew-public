7ed0e1a38edf5c56423062d4741ee454d16f2779
Johan Haals <johan.haals@gmail.com>
2018-03-28 10:09:45 +0200

    Add protoman

---
+++ protoman.rb
@@ -0,0 +1,11 @@
+class Protoman < Formula
+    desc "Protoman CLI tool"
+    homepage "https://github.com/spotify/protoman"
+    url "https://github.com/spotify/protoman/releases/download/0.0.1/protoman-darwin-amd64.zip"
+    version "0.0.1"
+    sha256 "451a43026d415bb709af6ac1eba9b4be1af4c3ad8e4b530773436693a32a051a"
+
+    def install
+      bin.install "protoman"
+    end
+  end
